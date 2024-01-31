b3184a95cb94e26d6c5862e7c6807ab12a3a45a3
De Jager, L, Mr [24908134@sun.ac.za] <24908134@sun.ac.za>
2024-01-31 19:42:00 +0200

    fixed remap import

--- nvim/lua/louiskop/init.lua
+++ nvim/lua/louiskop/init.lua
@@ -1,6 +1,7 @@
-require("louiskop.remap")
 -- gruvbox theme
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 -- line numbers & tabs
 require("louiskop.set")
+-- add remaps
+require("louiskop.remap")
